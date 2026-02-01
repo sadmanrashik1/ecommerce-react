c09bf57c7a50f35c94510fc6760809a04b9981da
unknown <artyomfreeman@outlook.com>
2026-02-01 21:30:11 +0000

    feat(cart): complete cart page UI and functionality
    - add CartItemsList and CartItem components
    
    - implement remove and edit item reducers
    
    - add amount dropdown with dynamic options
    
    - display formatted price and color preview
    
    - integrate toast messages for cart actions
    
    - update totals and localStorage on changes

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from 'react-router-dom'
-import { Navbar, Header } from '../components/Index'
+import { Outlet, useNavigation } from 'react-router-dom'
+import { Navbar, Header, Loading } from '../components'
 
 const HomeLayout = () => {
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === 'loading'
   return (
     <>
       <Header />
       <Navbar />
-      <section className='align-element py-20'>
-        <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading />
+      ) : (
+        <section className='align-element py-20'>
+          <Outlet />
+        </section>
+      )}
     </>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,30 @@
-const Products = () => {
-  return <h1 className='text-4xl'>Products</h1>
+import { Filters, PaginationContainer, ProductsContainer } from '../components'
+
+import { customFetch } from '../utils'
+
+const url = '/products'
+
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ])
+
+  const response = await customFetch(url, {
+    params,
+  })
+
+  const products = response.data.data
+  const meta = response.data.meta
+  return { products, meta, params }
 }
+const Products = () => {
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  )
+}
+
 export default Products
